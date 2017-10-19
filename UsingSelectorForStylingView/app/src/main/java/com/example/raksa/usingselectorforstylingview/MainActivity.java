258d1d5bd3e02a094836a0af81e745c243cc5cc8
BamBo <raksameth@gmail.com>
2017-10-19 12:44:27 +0700

    Add Styling Using Selector Demo

---
+++ UsingSelectorForStylingView/app/src/main/java/com/example/raksa/usingselectorforstylingview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.raksa.usingselectorforstylingview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+    }
+
+
+}
